26ca4a7a512e5d789bafe5174c5c050a41f5c677
asche1 <ascheie1@jhu.edu>
2020-03-13 11:42:35 -0400

    Consolidate, add cif_import
    Consolidate all the auxiliary files and scripts into a subdirectory.
    Add the functions for importing cif files

---
+++ pcf_lib/LatticeClass.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+class lattice:
+	def __init__(self,a,b,c,alpha,beta,gamma):
+		"""Define the unit cell"""
+		self.anorm = a
+		self.bnorm = b
+		self.cnorm = c
+		alpha = alpha*np.pi/180
+		beta = beta*np.pi/180
+		gamma = gamma*np.pi/180
+		self.a = a*np.array([1,0,0])
+		self.b = b*np.array([np.cos(gamma), np.sin(gamma), 0])
+		self.c = c*np.array([np.cos(beta), 
+			np.sin(beta)*np.cos(alpha)*np.sin(gamma), 
+			np.sin(alpha)*np.sin(beta) ])
+		#Round numbers (eliminate tiny numbers)
+		self.b=np.around(self.b,decimals=8)
+		self.c=np.around(self.c,decimals=8)
+
+		#Define the reciprocal lattice
+		self.reciplatt()
+
+	def reciplatt(self):
+		"""output the reciprocal lattice in np vectors"""
+		cellvol = np.dot(self.a,np.cross(self.b,self.c))
+
+		self.astar = 2*np.pi * np.cross(self.b,self.c) / cellvol
+		self.bstar = 2*np.pi * np.cross(self.c,self.a) / cellvol
+		self.cstar = 2*np.pi * np.cross(self.a,self.b) / cellvol
+
+		#Round numbers (eliminate tiny numbers)
+		self.bstar=np.around(self.bstar,decimals=8)
+		self.cstar=np.around(self.cstar,decimals=8)
+
+		self.V = cellvol
+		self.Vstar = np.dot(self.astar,np.cross(self.bstar,self.cstar))
+
+	def cartesian(self,vect,norm=True):
+		"""Convert a vector from ABC space to Cartesian Space"""
+		if vect.size == 3:
+			if norm == True:
+				return vect[0]*self.a + vect[1]*self.b + vect[2]*self.c
+			else:
+				return vect[0]*self.a/np.linalg.norm(self.a) \
+					+ vect[1]*self.b/np.linalg.norm(self.b) \
+					+ vect[2]*self.c/np.linalg.norm(self.c)
+		elif len(vect[0]) == 3:
+			return np.outer(vect[:,0],self.a) + np.outer(vect[:,1],self.b) + np.outer(vect[:,2],self.c)
+		else:
+			raise ValueError("vector must have three components") 
+
+
+	def ABC(self, vect):
+		"""Convert a vector from Cartesian Space to ABC space"""
+		matrix = np.array([self.a, self.b, self.c])
+
+		if vect.size == 3:
+			return np.dot(vect,np.linalg.inv(matrix))
+		else:
+			raise ValueError("vector must have three components") 
+
+
+	def inverseA(self,vect,norm=True):
+		"""Convert a vector from RLU space to inverse Aangstroms abs value"""
+		if vect.size == 3:
+			return vect[0]*self.astar + vect[1]*self.bstar + vect[2]*self.cstar
+		elif len(vect[0])==3:
+			return np.outer(vect[:,0],self.astar) + np.outer(vect[:,1],self.bstar) + np.outer(vect[:,2],self.cstar)
+		else:
+			raise ValueError("vector must have three components") 
---
+++ pcf_lib/form_factors.py
@@ -0,0 +1,82 @@
+import pickle
+import numpy as np
+#import time
+#from dataset import DataSet
+
+
+####################################################################################
+## Kemp's function
+def isotropic_ff(s,ion):
+    fhd = open('/home/1o1/Documents/Modules/3d_formfactors_j0.pck','rb')
+    form_factors_3d = pickle.load(fhd)
+    fhd.close()
+    
+    coefs = form_factors_3d[ion]
+    
+    j0 = coefs[0]*np.exp(-coefs[1]*s**2) + coefs[2]*np.exp(-coefs[3]*s**2)  + coefs[4]*np.exp(-coefs[5]*s**2) +coefs[6]
+
+    fhd = open('/home/1o1/Documents/Modules/3d_formfactors_j2.pck','rb')
+    form_factors_3d = pickle.load(fhd)
+    fhd.close()
+    
+    coefs = form_factors_3d[ion]
+    
+    j2 = s**2*(coefs[0]*np.exp(-coefs[1]*s**2) + coefs[2]*np.exp(-coefs[3]*s**2)  + coefs[4]*np.exp(-coefs[5]*s**2) +coefs[6])
+
+    return [j0,j2] 
+######################################################################################
+
+
+
+# My function (for now, only valid for rare earths.)
+
+Jion = {}   # [S, L, J]
+Jion['Ce3+'] = [0.5, 3., 2.5]
+Jion['Pr3+'] = [1., 5., 4.]
+Jion['Nd3+'] = [1.5, 6., 4.5]
+Jion['Pm3+'] = [2., 6., 4.]
+Jion['Sm3+'] = [2.5, 5, 2.5]
+Jion['Tb3+'] = [3., 3., 6.]
+Jion['Dy3+'] = [2.5, 5., 7.5]
+Jion['Ho3+'] = [2., 6., 8.]
+Jion['Er3+'] = [1.5, 6., 7.5]
+Jion['Tm3+'] = [1., 5., 6.]
+Jion['Yb3+'] = [0.5, 3., 3.5]
+
+def LandeGFactor(ion):
+    s, l, j = Jion[ion]
+    return 1.5 + (s*(s+1.) - l*(l+1.))/(2.*j*(j+1.))
+
+def importRE_FF(ion):
+    coefs = [[],[]]
+    j=0
+    for line in open('/home/1o1/Documents/Modules/RE_formfactors.pck'):
+        if not line.startswith(('#', ' ','\n')):
+            if line.split(' \t')[0] in ion:
+                coefs[j] = [float(i) for i in line.split(' \t')[1:]]
+                j+=1
+                #print line.split(' \t')
+    return coefs[0], coefs[1]
+
+def RE_FormFactor(magQ,ion):
+    """This uses the dipole approximation.
+    Note that Q must be a scalar in inverseA"""
+    #magQ = np.linalg.norm(lattice.inverseA(Q),axis=-1)
+    s = magQ/(4.*np.pi)
+    
+    coefs0, coefs2 = importRE_FF(ion)
+    
+    j0 = coefs0[0]*np.exp(-coefs0[1]*s**2) + coefs0[2]*np.exp(-coefs0[3]*s**2)  + \
+        coefs0[4]*np.exp(-coefs0[5]*s**2) +coefs0[6]
+
+    j2 = s**2*(coefs2[0]*np.exp(-coefs2[1]*s**2) + coefs2[2]*np.exp(-coefs2[3]*s**2)+\
+               coefs2[4]*np.exp(-coefs2[5]*s**2) + coefs2[6])
+
+    S, L, J = Jion[ion]
+     
+    j2factor = (J*(J+1.) - S*(S+1.) + L*(L+1.))/(3.*J*(J+1.) + S*(S+1.) - L*(L+1.))
+    return (j0 + j2*j2factor)**2
+
+#print importRE_FF('Yb3+')
+
+#print RE_FormFactor(np.arange(0,5,0.1),'Nd3+')

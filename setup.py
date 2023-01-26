b52b750a89fe4f8a899c365c78e15e731e1deae1
asche1 <scheie@lanl.gov>
2023-01-26 14:55:49 -0700

    Update setup.py
    New version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name='PyCrystalField',
-    version='2.3.5',    
+    version='2.3.6',    
     description='Code to calculate the crystal field Hamiltonian of magnetic ions.',
     url='https://github.com/asche1/PyCrystalField/tree/for_PyPi',
     author='Allen Scheie',
-    author_email='scheieao@ornl.gov',
+    author_email='scheie@lanl.gov',
     license='GNU GPL',
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),

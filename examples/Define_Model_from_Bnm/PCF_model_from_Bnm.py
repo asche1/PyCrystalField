b9b6d04c0f64f29eb48d382ab37a0006c18a7571
asche1 <scheie@lanl.gov>
2024-10-10 09:13:22 -0600

    Add files via upload

--- examples/Define_Model_from_Bnm/PCF_model_from_Bnm.py
+++ examples/Define_Model_from_Bnm/PCF_model_from_Bnm.py
@@ -6,6 +6,8 @@ previously defined CEF parameters rather than from a .cif
 or point charge model. 
 Allen Scheie, October 2024'''
 
+############################################
+## Example 1: Cubic symmetry
 
 # First, create a dictionary of the parameters. For negative m, define e.g. "B4-4". 
 Bnm = {'B20':-0.42, 'B40': 0.02, 'B44':0.13}  
@@ -15,4 +17,15 @@ Bnm = {'B20':-0.42, 'B40': 0.02, 'B44':0.13}
 Nd3 = cef.CFLevels.Bdict(ion='Nd3+', Bdict=Bnm)
 
 # Now, you can diagonalize, plot, fit, etc. just like any other PyCrystalField object. 
+Nd3.printEigenvectors()
+
+
+############################################
+##  Example 2: three-fold symmetry: 
+
+## Define Bnm
+Bnm = {'B20':0.22, 'B40': 0.03, 'B43':0.011, 'B60': -0.0002, 'B63':0.0001, 'B66':0.00054}  
+# Define Hamiltonian
+Nd3 = cef.CFLevels.Bdict(ion='Nd3+', Bdict=Bnm)
+# Print Eigenvectors
 Nd3.printEigenvectors()
